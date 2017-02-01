038079516da5409ed81f3cee228064ab4f50649a
Jay Freestone <mail@jayfreestone.com>
2017-02-01 07:42:17 +0000

    Integrates with API.

--- src/pages/Session.jsx
+++ src/pages/Session.jsx
@@ -1,28 +1,54 @@
 import React, { Component } from 'react';
+import _ from 'lodash/fp';
 
 class Session extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			board: '',
+			id: '',
+			posts: [],
+		};
+	}
 	componentDidMount() {
 		const params = this.props.router.params;
 
-		this.fetchThread({
+		this.setState({
 			board: params.board,
 			id: params.id,
+		}, () => {
+			this.fetchThread();
 		});
 	}
-	fetchThread(details) {
-		const request = new Request(`http://a.4cdn.org/${details.board}/thread/${details.id}.json`, {
+	fetchThread(details = this.state) {
+		const request = new Request('http://api.gauntlet.dev/proxy.php', {
+			method: 'GET',
 			headers: new Headers({
-				'Origin': 'http://boards.4chan.org'
+				'X-Proxy-URL': `http://a.4cdn.org/${details.board}/thread/${details.id}.json`,
+				'Content-Type': 'application/json',
 			}),
 		});
 
-		fetch(request);
+		const hasFile = _.filter((p) => p.filename);
+		const hasValidComment = _.filter((p) => /\+?\d+( |,| )+[a-z]+( |,| )+[a-z]+/.test(p.com));
+
+		fetch(request)
+			.then(resp => resp.json())
+			.then((data) => {
+				const validPosts = _.compose(hasValidComment, hasFile);
+				this.setState({ posts: validPosts(data.posts) });
+		});
 	}
   render() {
     return (
-      <div>
-				Nice session...
-      </div>
+      <ul>
+				{this.state.posts.map((post) => (
+					<li key={post.no}>
+						<img src={`http://is2.4chan.org/${this.state.board}/${post.tim}${post.ext}`} alt=""/>
+					</li>
+				))}
+      </ul>
     );
   }
 }
